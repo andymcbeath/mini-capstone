887e65bb5c3780f2748422391e621a0bc99089d3
amanda <amanda@actualize.co>
2022-06-30 13:36:53 -0500

    creates routes and controller actions for second and third products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,4 +8,14 @@ class ProductsController < ApplicationController
     product = Product.first
     render json: product.as_json
   end
+
+  def second_product
+    product = Product.second
+    render json: product.as_json
+  end
+
+  def third_product
+    product = Product.third
+    render json: product.as_json
+  end
 end
